2aac9c04b46b3f445cd1e4f80b96ab3a7bc99c72
Michael Wixson <wixsonmc@gmail.com>
2020-02-05 22:26:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,8 @@ def monopoly_with_third_tier
   # complexity.
   {railroads: {pieces: 4}, 
      {rent_in_dollars: {
-        one_piece_owned: 25,
+        one_piece_owned: "25",
+        two_pieces_owned: "50",
         
   }}}
 end
